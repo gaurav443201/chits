0cdd59f3a942484e7ee76f20e8a9e8fe62ecda1e
CRAZYSHIT <NAVGHAREGAURAV80@GMAIL.COM>
2025-11-14 01:33:13 +0530

    Update 19.py

--- 19.py
+++ 19.py
@@ -1,56 +1,85 @@
-def insert(root, key):
-    if root is None:
+# ----------- Binary Search Tree (BST) -----------
+
+root = None  # Global root node
+
+def insert(key):
+    global root
+    root = insert_node(root, key)
+
+def insert_node(node, key):
+    if node is None:
         return [key, None, None]  # [data, left, right]
 
-    if key < root[0]:
-        root[1] = insert(root[1], key)
-    elif key > root[0]:
-        root[2] = insert(root[2], key)
+    if key < node[0]:
+        node[1] = insert_node(node[1], key)
+    elif key > node[0]:
+        node[2] = insert_node(node[2], key)
     else:
         print("Duplicate key not allowed!")
-    return root
+    return node
 
-def search(root, key):
+
+def search():
+    global root
     if root is None:
+        print("Tree is empty!")
+        return
+    key = int(input("Enter key to search: "))
+    search_node(root, key)
+
+def search_node(node, key):
+    if node is None:
         print("Key not found.")
         return
-    if key == root[0]:
+    if key == node[0]:
         print("Key found:", key)
-    elif key < root[0]:
-        search(root[1], key)
+    elif key < node[0]:
+        search_node(node[1], key)
     else:
-        search(root[2], key)
+        search_node(node[2], key)
 
-def inorder(root):
-    if root is not None:
-        inorder(root[1])
-        print(root[0], end=" ")
-        inorder(root[2])
 
-# ------------------------------------
-root = None
+def inorder():
+    global root
+    if root is None:
+        print("Tree is empty!")
+        return
+    print("Inorder Traversal:", end=" ")
+    inorder_traversal(root)
+    print()
 
-while True:
+def inorder_traversal(node):
+    if node is not None:
+        inorder_traversal(node[1])
+        print(node[0], end=" ")
+        inorder_traversal(node[2])
+
+
+# ------------------ MAIN MENU ------------------
+def menu():
     print("""
-Menu:
+===== MENU =====
 1) Insert
 2) Search
 3) Display (Inorder)
 4) Exit
+================
 """)
+
+
+while True:
+    menu()
     ch = int(input("Enter your choice: "))
-    
+
     if ch == 1:
         key = int(input("Enter key to insert: "))
-        root = insert(root, key)
+        insert(key)
     elif ch == 2:
-        key = int(input("Enter key to search: "))
-        search(root, key)
+        search()
     elif ch == 3:
-        print("Inorder Traversal:", end=" ")
-        inorder(root)
-        print()
+        inorder()
     elif ch == 4:
+        print("THANK YOU!!!")
         break
     else:
-        print("Invalid choice!")
+        print("Invalid choice! Please enter 1–4.")
